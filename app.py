abc6377fbc46c6c4514300640d1d53e617c283c6
mbortkiewicz <michalbortkiewicz8@gmail.com>
2020-06-17 11:54:57 +0200

    change to polish tab2

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def update_output(n_clicks, input1, input2, input3):
     MAE = low_numbers_15_06_query_values - input_values
     if n_clicks > 0:
         return u'''
-            Errors for Bahamas deaths: {}, Brunei confirmed cases: {}, Burma recovered: {}
+            Błąd dla liczby zgonów na Bahama: {}, liczby potwierdzonych przypadków w Brunei: {}, liczby wyleczonych w Burmie: {}
         '''.format(MAE[0], MAE[1], MAE[2])
 
 @app.callback(Output('output_bar2', 'children'),
@@ -112,7 +112,7 @@ def update_output(n_clicks, input1, input2, input3):
     MAE = low_numbers_15_06_query_values - input_values
     if n_clicks > 0:
         return u'''
-            Errors for Bahamas deaths: {}, Brunei confirmed cases: {}, Burma recovered: {}
+            Błąd dla liczby zgonów na Bahama: {}, liczby potwierdzonych przypadków w Brunei: {}, liczby wyleczonych w Burmie: {}
         '''.format(MAE[0], MAE[1], MAE[2])
 
 
